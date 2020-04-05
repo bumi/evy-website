38a86e0e35fcf50923cc16cf9c6318bd4d7a2ddb
Michael Bumann <hello@michaelbumann.com>
2020-04-05 10:16:29 +0200

    publish script

---
+++ publish.sh
@@ -0,0 +1,11 @@
+echo "Removing existing files"
+rm -rf public/*
+
+echo "Generating site"
+hugo
+
+echo "Updating gh-pages branch"
+cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+
+echo "Pushing to github"
+git push --all
