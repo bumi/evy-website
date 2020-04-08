89a311e8b4a136f5eda97de4eedccbf9abff5912
Michael Bumann <hello@michaelbumann.com>
2020-04-08 16:17:56 +0200

    Add stoff selection to order form

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,4 @@
 "use strict";
-"use strict";
 
 var App = {
   init: function() {
@@ -8,6 +7,15 @@ var App = {
     if (orderForm) {
       orderForm.addEventListener('submit', App.submitOrder);
     }
+
+    var images = document.querySelectorAll('.order-samples img');
+    images.forEach(function(img) {
+      console.log(img);
+      img.addEventListener('click', App.zoomImage);
+    });
+    document.getElementById("lightbox-back").addEventListener("click", function(){
+      this.classList.remove("show");
+    });
   },
   initBurgerHandler: function() {
     var navContainer = document.querySelector('.nav-container');
@@ -18,28 +26,45 @@ var App = {
     });
   },
 
+  zoomImage: function(e) {
+    console.log(e);
+    e.preventDefault();
+    // Create evil image clone
+    console.log(this);
+
+    var img = document.createElement('img');
+    img.src = this.dataset.target;
+    var lb = document.getElementById("lightbox-content");
+    lb.innerHTML = "";
+    lb.appendChild(img);
+
+    // Show lightbox
+    lb = document.getElementById("lightbox-back");
+    lb.classList.add("show");
+
+  },
+
   submitOrder: function(e) {
     e.preventDefault();
     var url = this.getAttribute('action');
-    var data = {};
-    this.querySelectorAll('input').forEach(function(field) {
-      data[field.getAttribute('name')] = field.value;
-    });
+    var data = new FormData(this);
+
     var button = this.querySelector('button');
     button.disabled = true;
-    button.innerHTML = '...einen Moment';
+    button.innerHTML = '...einen Moment bitte';
 
-    fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      },
-      body: JSON.stringify(data)
-    })
-      .then(function(r) { return r.json() })
-      .then(function(r) {
-        if(r.success) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', url, true);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+      if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+        if (this.status !== 200) {
+          alert('Es ist leider ein Fehler aufgetreten. Bitte versuche es erneut');
+          return;
+        }
+        var response = JSON.parse(xhr.responseText);
+        // Request finished. Do processing here.
+        if(response.success) {
           var anfragen = document.getElementById('anfragen');
           anfragen.scrollIntoView();
           scrollBy(0, -200); // scroll a bit further up
@@ -47,10 +72,13 @@ var App = {
         } else {
           alert('Es ist leider ein Fehler aufgetreten. Bitte versuche es erneut');
         }
-      });
+      }
+    };
+    xhr.send(data);
   }
-
 };
 
-App.init();
+window.addEventListener("load", function(){
+  App.init();
+});
 
